967d3531e005ece1521a54de1b876fca80113581
wangyingbo <wangyingbo528@126.com>
2020-07-14 10:47:15 +0800

    add checkkeys.sh

---
+++ demo/Linux就该这么学demo/Checkkeys.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# author wangyingbo
+
+read -p "请输入一个字符，并按 Enter 键确认：" KEY 
+case "$KEY" in 
+	[a-z]|[A-Z]) 
+	echo "您输入的是 字母。" 
+	;; 
+	[0-9]) 
+	echo "您输入的是 数字。" 
+	;; 
+	*) 
+	echo "您输入的是 空格、功能键或其他控制字符。" 
+esac
