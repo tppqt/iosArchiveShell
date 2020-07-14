f76a67434dcc63b4daff5aeaf53ed0fadfefb836
wangyingbo <wangyingbo528@126.com>
2020-07-14 11:02:26 +0800

    add chkhost.sh

---
+++ demo/Linux就该这么学demo/CheckScore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# author wangyingbo
+
+read -p "Enter your score（0-100）：" GRADE 
+if [ $GRADE -ge 85 ] && [ $GRADE -le 100 ] ; then 
+	echo "$GRADE is Excellent" 
+elif [ $GRADE -ge 70 ] && [ $GRADE -le 84 ] ; then 
+	echo "$GRADE is Pass" else echo "$GRADE is Fail" 
+fi
